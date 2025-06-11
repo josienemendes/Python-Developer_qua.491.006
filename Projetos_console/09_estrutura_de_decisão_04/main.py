94c72ffdcf28f0e1a477797ab7631a1f38e7c38c
josienemendes <jossymendess123@gmail.com>
2025-06-11 19:52:36 -0300

    Primeiro Commit. Aula do dia 11/06/2025

---
+++ Projetos_console/09_estrutura_de_decisão_04/main.py
@@ -0,0 +1,18 @@
+import os
+os.system
+
+#declaração de variáveis
+
+nome = input("Informe seu nome: ")
+idade = int(input("Informe sua idade: "))
+
+#ternario
+result = "é maior de idade." if idade >= 18 else "é menor de idade."
+
+#sáida
+print(f"{nome} {result}")
+
+
+
+
+
